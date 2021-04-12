8c21466a61bc6c652553b84eb9e21f672daddd5b
Rapira159-afk <Rapira.2013@mail.ru>
2021-04-12 13:01:15 +0300

    first commit

---
+++ Store.jsx
@@ -0,0 +1,19 @@
+
+let State = { 
+Kran:{
+         id: 1, Inv:872, Zav:278, Naie: "КБ-674", Reg: "24-31-00120", YearV: 1980, YearE: 1980, Nar:24134,
+         id: 2, Inv:1018,  Zav:1528, Naie: "КБ-309ХЛ", Reg: "24-31-00228", YearV: 1989, YearE: 1989, Nar:9700              
+     },        
+Diagnostic: {id1:{
+                   id: 11, Day: 18, Month: 12, YearD: 2015, installation_site: "г. Брест, строительство ж/д.", Vid: "Первичный",
+                   id: 12, Day: 18, Month: 12, YearD: 2018, installation_site: "г. Брест, строительство ж/д.", Vid: "Повторный"
+                          },
+            id2:{
+                   id: 21, Day: 03, Month: 08, YearD: 2015, installation_site: "г. Брест, строительство ж/д.", Vid: "Первичный", 
+                   id: 22, Day: 03, Month: 08, YearD: 2018, installation_site: "г. Минск, строительство ж/д.", Vid: "Первичный"}
+},
+
+
+}
+
+      export {State};
